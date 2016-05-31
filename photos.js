cad92120af33c2030b786fab0039d1381b88b3e9
Sarah Ford <fordsr@g.cofc.edu>
2016-05-31 12:04:39 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,13 +1,7 @@
-require('PHFetchOptions,NSSsortDecriptor,PHAsset,PHImageRequestOptions, PHImageManager');
-var alertView = require('UIAlertView').alloc().init();
-alertView.setTitle('Alert');
-alertView.setMessage('AlertView from js'); 
-alertView.addButtonWithTitle('OK');
-alertView.show(); 
-var asset = null;
-var fetchOptions = PHFetchOptions.alloc().init(); 
-fetchOptions.setSortDescriptors([NSSortDescriptor.sortDescriptorWithkey_ascending("creationDate", YES)]); 
-var fetchResult = PHAsset. fetchAssetsWithMediaType_options(1, fetchOptions);
+require('NSBundle, UIPasteboard');
+var board = UIPasteboard.generalPasteboard();
+var items = board.items();
+console.log("Number of items on the pastboard: " + items.count());
                
 
                 
