b9cc40d87f31b25df33796ffdcaf9fd4840845d7
Sarah Ford <fordsr@g.cofc.edu>
2016-05-31 11:40:32 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -6,5 +6,11 @@ alertView.addButtonWithTitle('OK');
 alertView.show(); 
 var asset = null;
 var fetchOptions = PHFetchOptions.alloc().init();
-fetchOptions.setSortDescriptors[NSSortDescripto.sortDescriptorWithKey_ascending("creationDate", YES)]);
+fetchOptions.setSortDescriptors([NSSortDescripto.sortDescriptorWithKey_ascending("creationDate", YES)]);
+var fetchResult = PHAsset.fetchAssetsWithMediaType_options(1, fetchOptions);
+if (fetchResult != null && fetchResult.count() > 0) {
+  asset = fetchResult.lastObject();
+} else {
+  console.log("NO asset");
+}
                 
