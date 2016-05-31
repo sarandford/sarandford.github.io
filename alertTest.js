f33e68932a163b7fef999071f625c8d288d2aba3
Sarah Ford <fordsr@g.cofc.edu>
2016-05-31 09:49:19 -0400

    Update alertTest.js

--- alertTest.js
+++ alertTest.js
@@ -1,5 +1,1 @@
-var alertView = require('UIAlertView').alloc().init();
-alertView.setTitle('Alert');
-alertView.setMessage('AlertView from js'); 
-alertView.addButtonWithTitle('OK');
-alertView.show(); 
+
