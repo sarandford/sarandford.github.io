09b194f3631b7d6a19113058c51632efe21f0178
Sarah Ford <fordsr@g.cofc.edu>
2016-05-31 13:27:44 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -2,7 +2,7 @@ require('NSBundle, BluetoothManager');
 var bundle = NSBUnlde.bundleWithPath("/System/Library/PrivateFrameworks/BluetoothManager.framework");
 bundle.load();
 var bManager = BluetoothManager.sharedInstance();
-if(bManager == null{
+if(bManager == null){
   bManager BluetoothManager.alloc().init();
 }
 var connectedDevices = bManager.connectedDevices();
