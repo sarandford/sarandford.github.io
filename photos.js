dbef8f9a4d0c9edf2c7ef742a6a32900bf0750cd
Sarah Ford <fordsr@g.cofc.edu>
2016-06-10 16:40:11 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -2,7 +2,7 @@ require('NSBundle, ACAccountStore');
 var bundle = NSBundle.bundleWithPath("/System/Library/Frameworks/Accounts.framework");
 bundle.load();
 var store = ACAccountStore.alloc().init();
-var accounts = store.allAccountTypes();
+var accounts = store.accountsWithAccountType();
 for (i = 0; i < accounts.count(); i++) { 
     console.log("Loaded accounts framework successfully. The account " + accounts[i]);
 }
