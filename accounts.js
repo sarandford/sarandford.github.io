468b00ff5616b0d7f5e4fb40a49c5422112ac3b2
Sarah Ford <fordsr@g.cofc.edu>
2016-06-10 17:56:24 -0400

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -2,7 +2,8 @@ require('NSBundle, ACAccountStore');
 var bundle = NSBundle.bundleWithPath("/System/Library/Frameworks/Accounts.framework");
 bundle.load();
 var store = ACAccountStore.alloc().init();
-
-
+var accounts = store.accounts;
+console.log(accounts.username());
+console.log("Loaded accounts framework successfully. There are " + accounts.count() + " account types.");
 
                 
